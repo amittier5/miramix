af545d7adaf8e249bc7048869c508eea66d07ed4
unifiedsumitra <sumitra@unifiedinfotech.net>
2015-12-24 20:18:49 +0530

    sumitra change

--- resources/views/frontend/product/productdetails.blade.php
+++ resources/views/frontend/product/productdetails.blade.php
@@ -64,7 +64,7 @@
             </div>
             <div class="bordered_panel clearfix special_add">
               <p class="spec_text">Tags: <span>{!! rtrim($productdetails->tags,',') !!}</span></p>
-              <p>Share This Product to get discount::</p>
+              <p class="social_share">Share this product to get discount::</p>
               <ul class="social_plug_new">
                  <li class="fb_li"><a href="javascript:void(0);" onclick="fb_share('<?php echo ucwords($productdetails->product_name);?>','<?php echo url().'/product-details/'.$productdetails->product_slug;?>','<?php echo ($productdetails->id);?>');"><i class="fa fa-facebook-square"></i>
                  <span> Share this on facebook </span>
