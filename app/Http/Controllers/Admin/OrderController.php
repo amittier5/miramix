ae0b23546070b8f2fe2c3de7e37f06cb1c1c9376
amitunifiedinfotech <amit@unifiedinfotech.net>
2015-12-28 19:26:30 +0530

    change in usps

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -345,14 +345,14 @@ public function update(Request $request, $id)
 				$setting = DB::table('sitesettings')->where('name', 'email')->first();
 				$admin_users_email=$setting->value;
 				
-				// $sent = Mail::send('admin.order.statusemail', array('name'=>$user_name,'email'=>$user_email,'messages'=>$cmessage,'admin_users_email'=>$admin_users_email,'tracking'=>$tracking,'shipping'=>$shipping), 
+				$sent = Mail::send('admin.order.statusemail', array('name'=>$user_name,'email'=>$user_email,'messages'=>$cmessage,'admin_users_email'=>$admin_users_email,'tracking'=>$tracking,'shipping'=>$shipping), 
 				
-				// function($message) use ($admin_users_email, $user_email,$user_name,$subject)
-				// {
-				// 	$message->from($admin_users_email);
-				// 	$message->to($user_email, $user_name)->cc($admin_users_email)->subject($subject);
+				function($message) use ($admin_users_email, $user_email,$user_name,$subject)
+				{
+					$message->from($admin_users_email);
+					$message->to($user_email, $user_name)->cc($admin_users_email)->subject($subject);
 					
-				// });
+				});
 
         	}
         }
