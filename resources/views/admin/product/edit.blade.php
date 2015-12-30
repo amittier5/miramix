391a9ccc0392040aadfcb73068d58c5f7bf585ca
ayan-c <ayan.c@unifiedinfotech.net>
2015-12-30 20:00:38 +0530

    changes add

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -622,8 +622,7 @@ var total_value = parseFloat(this_vald) * parseFloat($this.parent().parent().fin
           data: { ingredient_id : ingredient_id  ,_token: '{!! csrf_token() !!}',ingredient_text : ingredient_text,id_tr : id_tr},
           success:function(data)
           {
-            if(data !='' ) 
-            {
+            
 			$('#load_table').show();
 			$('.form_check_table table').css({'opacity':0});
 			
@@ -670,7 +669,7 @@ var total_value = parseFloat(this_vald) * parseFloat($this.parent().parent().fin
 			 	checktable_val(); 
 			  
 			  
-            }
+            
           }
       });
 
