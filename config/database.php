6a993eae9202bcfa6aec75d536db98787662d7e0
unifiedsumitra <sumitra@unifiedinfotech.net>
2015-12-31 12:54:54 +0530

    db

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
            'host'      => env('DB_HOST', 'localhost'),
            'database'  => env('DB_DATABASE', 'miramix'),
            'username'  => env('DB_USERNAME', 'root'),
-           'password'  => env('DB_PASSWORD', ''),
+           'password'  => env('DB_PASSWORD', 'user123'),
            'charset'   => 'utf8',
            'collation' => 'utf8_unicode_ci',
            'prefix'    => '',
